81af29fc4d605e94a77ef8f9388e799b34f2eb09
Robotv2 <81443374+Robotv2@users.noreply.github.com>
2023-09-04 00:51:33 +0200

    Added support for hex color in all strings

--- bukkit/src/main/java/fr/robotv2/bukkit/util/text/ColorUtil.java
+++ bukkit/src/main/java/fr/robotv2/bukkit/util/text/ColorUtil.java
@@ -1,6 +1,9 @@
 package fr.robotv2.bukkit.util.text;
 
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ColorUtil {
 
@@ -8,8 +11,36 @@ public class ColorUtil {
 
     private ColorUtil() { }
 
-    public static String color(String message) {
-        return ChatColor.translateAlternateColorCodes('&', message);
+    private final static Pattern HEX_PATTERN = Pattern.compile("#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})");
+
+    public static String color(String text) {
+        text = ChatColor.translateAlternateColorCodes('&', text);
+        text = hexColor(text);
+        return text;
     }
 
+    public static String hexColor(String text) {
+
+        if(!text.contains("#")) {
+            return text;
+        }
+
+        Matcher matcher = HEX_PATTERN.matcher(text);
+        StringBuffer buffer = new StringBuffer(text.length());
+
+        if(!matcher.find()) {
+            return text;
+        }
+
+        matcher.reset();
+
+        while (matcher.find()) {
+            String colorCode = matcher.group();
+            ChatColor color = ChatColor.of(colorCode);
+            matcher.appendReplacement(buffer, color.toString());
+        }
+
+        matcher.appendTail(buffer);
+        return buffer.toString();
+    }
 }
