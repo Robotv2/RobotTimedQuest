b6c690898d8c427aba49ccf4f06648afd0a26fe1
Robotv2 <81443374+Robotv2@users.noreply.github.com>
2023-10-18 15:59:49 +0200

    type class

--- bukkit/src/main/java/fr/robotv2/bukkit/quest/custom/CustomQuestProgressionEnhancer.java
+++ bukkit/src/main/java/fr/robotv2/bukkit/quest/custom/CustomQuestProgressionEnhancer.java
@@ -18,15 +18,15 @@ public abstract class CustomQuestProgressionEnhancer<T> extends QuestProgression
         super(RTQ_PLUGIN);
     }
 
-    public boolean trigger(Player player, String name, Object target) {
+    public boolean trigger(Player player, String name, T target) {
         return this.trigger(player, name, target, 1);
     }
 
-    public boolean trigger(Player player, String name, Object target, int amount) {
+    public boolean trigger(Player player, String name, T target, int amount) {
         return this.trigger(player, name, target, null, amount);
     }
 
-    public boolean trigger(Player player, String name, Object target, Event event, int amount) {
+    public boolean trigger(Player player, String name, T target, Event event, int amount) {
 
         RTQ_PLUGIN.debug("Custom type '%s' has been triggered by %s", name, player.getName());
 
